1af4fbf4f68dcf85dcd2ef40790a1488f649f691
Zak McRae <zakmcrae@gmail.com>
2021-08-25 12:05:50 -0400

    added font, temp homepage

--- src/components/home/homepage.js
+++ src/components/home/homepage.js
@@ -1,9 +1,15 @@
 import "./homepage.css";
+import img from "./tools.jpg";
 
 const HomePage = () => {
   return (
     <div className="home-page">
-      <h1>Welcome</h1>
+      {/* Photo by <a href="https://unsplash.com/@jmolliver?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Julie Molliver</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+       */}
+      <img src={img} alt="" />
+      <div className="text-section">
+        <h1>Welcome to the Tool Store</h1>
+      </div>
     </div>
   );
 };

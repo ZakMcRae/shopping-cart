d8a93125f5bf6221fcca4d9ccc581e5c0e14e247
Zak McRae <zakmcrae@gmail.com>
2021-08-27 11:46:45 -0400

    more visual styling and fonts

--- src/components/home/homepage.js
+++ src/components/home/homepage.js
@@ -1,14 +1,20 @@
 import "./homepage.css";
-import img from "./tools.jpg";
+import coverImage from "./cover.webp";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
     <div className="home-page">
       {/* Photo by <a href="https://unsplash.com/@jmolliver?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Julie Molliver</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
        */}
-      <img src={img} alt="" />
+      <img src={coverImage} alt="" />
+      <h1 className="store-name">The Tool Store</h1>
       <div className="text-section">
-        <h1>Welcome to the Tool Store</h1>
+        <h2>We have right the tools for the job</h2>
+        <Link to="/products" className="browse-button">
+          Shop Now
+        </Link>
+        {/* <button>Shop Now</button> */}
       </div>
     </div>
   );

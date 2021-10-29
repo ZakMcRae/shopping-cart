9a412931dbc7b5012b56782791007d37e39a82b6
Zak McRae <zakmcrae@gmail.com>
2021-10-29 12:25:09 -0400

    improved responsive design of homepage
    store-name and text-section were overlapping on small screens

--- src/components/home/homepage.js
+++ src/components/home/homepage.js
@@ -8,13 +8,14 @@ const HomePage = () => {
       {/* Photo by <a href="https://unsplash.com/@jmolliver?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Julie Molliver</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
        */}
       <img src={coverImage} alt="" />
-      <h1 className="store-name">The Tool Store</h1>
-      <div className="text-section">
-        <h2>We have right the tools for the job</h2>
-        <Link to="/products" className="browse-button">
-          Shop Now
-        </Link>
-        {/* <button>Shop Now</button> */}
+      <div className="overlay">
+        <h1 className="store-name">The Tool Store</h1>
+        <div className="text-section">
+          <h2>We have right the tools for the job</h2>
+          <Link to="/products" className="browse-button">
+            Shop Now
+          </Link>
+        </div>
       </div>
     </div>
   );
